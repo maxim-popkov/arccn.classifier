d21d53277bdcc8eb1f577a08040f49a8a4135a20
maxim-popkov <maxim.popkov@outlook.com>
2014-08-13 21:47:29 +0400

    переводы

--- arccn.classifier/app_classifier/models.py
+++ arccn.classifier/app_classifier/models.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-
+# from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 
 class Classifier(models.Model):
 
     """
     DataBase ORM Model for Classifier
     """
-    title = models.CharField(max_length=64)
-    desc = models.TextField()
-    save_file_path = models.CharField(max_length=64, null=True, blank=True)
-    is_trained = models.BooleanField(default=False)
+    title = models.CharField(max_length=64, verbose_name=u"Название")
+    desc = models.TextField(verbose_name=u"Описание")
+    save_file_path = models.CharField(max_length=64, null=True, blank=True, verbose_name=u"Путь к файлам")
+    is_trained = models.BooleanField(default=False, verbose_name=u"Обучен?")
 
     class Meta:
         verbose_name = u'Классификатор'
@@ -28,9 +29,9 @@ class Label(models.Model):
     """
     DataBase ORM Model for Label
     """
-    assigned_id = models.CharField(max_length=64, blank=True)    
-    name = models.CharField(max_length=64)
-    cls = models.ForeignKey(Classifier)
+    assigned_id = models.CharField(max_length=64, blank=True, verbose_name=u"клиентский id")    
+    name = models.CharField(max_length=64, verbose_name=u"Название")
+    cls = models.ForeignKey(Classifier, verbose_name=u"Классификатор")
 
     class Meta:
         verbose_name = u'Категория'
@@ -45,11 +46,11 @@ class TrainVector(models.Model):
     """
     DataBase ORM Model for Train Vector
     """
-    assigned_id = models.CharField(max_length=64, blank=True)
-    cls = models.ForeignKey(Classifier)
-    lbl = models.ForeignKey(Label)
-    data = models.TextField()
-    title = models.CharField(max_length=512, blank=True)
+    assigned_id = models.CharField(max_length=64, blank=True, verbose_name=u"Клиентский id")
+    cls = models.ForeignKey(Classifier, verbose_name=u"Классификатор")
+    lbl = models.ForeignKey(Label, verbose_name=u"Категория")
+    data = models.TextField(verbose_name=u"Термы")
+    title = models.CharField(max_length=512, blank=True, verbose_name=u"Название")
 
     class Meta:
         verbose_name = u'Обучающий вектор'
@@ -71,14 +72,14 @@ class TestVector(models.Model):
     
     # param blank=True for admin panel,
     # with param can add vector without specifying foreignkeys
-    assigned_id = models.CharField(max_length=64, blank=True)
+    assigned_id = models.CharField(max_length=64, blank=True, verbose_name=u"Клиентский id")
     cls = models.ForeignKey(
-        Classifier, null=True, blank=True, db_constraint=False)
-    lbl = models.ForeignKey(Label, null=True, blank=True, db_constraint=False)
-    data = models.TextField()
-    isClassified = models.BooleanField(default=False)
-    accepted = models.BooleanField(default=False)
-    title = models.CharField(max_length=512, blank=True)
+        Classifier, null=True, blank=True, db_constraint=False, verbose_name=u"Классификатор")
+    lbl = models.ForeignKey(Label, null=True, blank=True, db_constraint=False, verbose_name=u"Категория")
+    data = models.TextField(verbose_name=u"Термы")
+    isClassified = models.BooleanField(default=False, verbose_name=u"Классифицирован?")
+    accepted = models.BooleanField(default=False, verbose_name=u"Одобрен?")
+    title = models.CharField(max_length=512, blank=True, verbose_name=u"Название")
 
 
     class Meta:
